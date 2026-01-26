98e5276edb20bbcd184141c53001d8d4ed2464ba
VikasKumar <86155964+vikaskumarorg@users.noreply.github.com>
2026-01-26 16:13:29 +0000

    state

--- dayone/src/profession-group.js
+++ dayone/src/profession-group.js
@@ -1,24 +1,24 @@
 import AddProfession from "./AddProfession";
+import { useState } from "react";
 
-function Professions({users, heading}){
-var index = 1;
-    function profession(){
-        index++;
-        users.push({
-            id: (index) +"_"+ heading,
+function Professions({users: initialUsers, heading}){
+const [users, setUsers] = useState(initialUsers);
+var i = 1;
+    function fnProfession(){
+        i = i+6;
+        setUsers([...users, {
+            id: i +"_"+ heading,
             name: 'Almanss  Katherine Johnson',
             profession: heading,
-        })
-
-        
+        }])
     }
     return (
         <>
         <h2>{heading}</h2>
         <ul>
-        {users.map(u => <li key="{u.id}">{u.name}</li>)}
+        {users.map((u)=> <li key={u.id}>{u.name}</li>)}
         </ul>
-        <AddProfession onClickProfession={profession} heading={heading}></AddProfession>
+        <AddProfession onClickProfession={fnProfession} heading={heading}></AddProfession>
         </>
     )
 
