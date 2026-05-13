2367938aa23c895bfdaf810ef15e441c3194a4cc
VikasKumar <86155964+vikaskumarorg@users.noreply.github.com>
2026-05-13 05:57:37 +0000

    Use effect hook

---
+++ hooks/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Effect from './Effect';
+
+function App() {
+  return (
+    <div className="App">
+    <Effect />
+    </div>
+  );
+}
+
+export default App;
---
+++ hooks/src/Effect.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function Effect() {
+  const [count, setCount] = useState(234567);
+  const pRef = useRef();
+
+  useEffect(() => {
+    if (pRef.current) {
+      pRef.current.style.backgroundColor = `#${count.toString(16).padStart(6, '0')}`;
+    }
+  }, [count]);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 4000)}>Change color</button>
+      <p ref={pRef} style={{color:'#fff'}}>Refresh background color</p>
+    </div>
+  );
+}
+
+export default Effect;
